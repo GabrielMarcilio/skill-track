4283d06435057ccaa29639674c0c1ae6c417a337
Gabriel Marcilio <gaheris.sc@gmail.com>
2016-06-29 21:00:33 -0300

    Added tests for person class

--- source/tutorials/spec/person_spec.js
+++ source/tutorials/spec/person_spec.js
@@ -1,5 +1,26 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
+describe("Person Suite", function() {
+
+    it("Check Constructor", function(){
+	    var mike = new Person('Mike', 25);
+	    
+	    expect(mike.name).toBe('Mike');
+	    expect(mike.age).toBe(25);
+    });
+});
+
+describe("Yet Another Suite", function() {
+	beforeEach(function() {
+		this.person = new Person("Oscar", 40);
+	});
+	
+	it("Testing setup", function(){
+		expect(this.person.name).toBe('Oscar');
+	    expect(this.person.age).toBe(40);
+	});
+	it("Initialized values can change", function(){
+		this.person.name = 'Mary';
+		expect(this.person.name).toBe('Mary');
+		expect(this.person.age).toBe(40);
+	});
+	
 });
