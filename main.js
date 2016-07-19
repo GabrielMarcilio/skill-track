0564b031a212c1e53bb94b5b70bed99e5455c5b3
Gabriel Marcilio <gaheris.sc@gmail.com>
2016-07-19 20:41:40 -0300

    Changed to use open shift port and ips preferentially
    - This is an attempt to fix an error when starting the app in open shift

--- main.js
+++ main.js
@@ -7,10 +7,14 @@ var serveStatic = require('serve-static');
 
 var serve = serveStatic("./");
 
+var port = process.env.OPENSHIFT_NODEJS_PORT  || 8080;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+
 var server = http.createServer(function(req, res) {
 	
     var done = finalhandler(req, res);
     serve(req, res, done);
 });
 
-server.listen(3000);
+console.log('Serving: ' + ipaddress + ' ' + port)
+server.listen(port, ipaddress);
